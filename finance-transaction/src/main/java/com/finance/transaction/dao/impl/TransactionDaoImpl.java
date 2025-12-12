0c1786904a51702f7f53cfa0c1b6730e9c540b25
Anshu Yadav <your@email.com>
2025-12-12 17:14:01 +0530

    added java file

---
+++ finance-transaction/src/main/java/com/finance/transaction/dao/impl/TransactionDaoImpl.java
@@ -0,0 +1,34 @@
+package com.finance.transaction.dao.impl;
+
+import com.finance.core.entity.Transaction;
+import com.finance.persistence.util.HibernateUtil;
+import com.finance.transaction.dao.TransactionDao;
+import org.hibernate.Session;
+import org.hibernate.Transaction as HbTx;
+
+import java.util.List;
+
+public class TransactionDaoImpl implements TransactionDao {
+
+    @Override
+    public Transaction save(Transaction tx) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        HbTx hbTx = session.beginTransaction();
+        session.save(tx);
+        hbTx.commit();
+        session.close();
+        return tx;
+    }
+
+    @Override
+    public List<Transaction> findByAccountNumber(String accountNumber) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<Transaction> list = session.createQuery(
+                "select t from Transaction t join t.account a where a.accountNumber = :acc order by t.createdAt desc",
+                Transaction.class)
+                .setParameter("acc", accountNumber)
+                .getResultList();
+        session.close();
+        return list;
+    }
+}
